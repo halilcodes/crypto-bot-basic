ae2261316a472df37533cefbfa6b0faac7a8c065
Halil_Hasmer <halilpython@gmail.com>
2022-06-15 12:21:07 +0300

    begin watchlist in ui

--- interface/root_component.py
+++ interface/root_component.py
@@ -11,6 +11,7 @@ from interface.styling import *
 from interface.logging_component import Logging
 from connectors.bitmex import BitmexClient
 from connectors.binance_futures import BinanceFuturesClient
+from interface.watchlist_component import WatchList
 
 
 class Root(tk.Tk):
@@ -29,12 +30,18 @@ class Root(tk.Tk):
         self._right_frame = tk.Frame(self, bg=BG_COLOR)
         self._right_frame.pack(side=tk.RIGHT)
 
+        self._watchlist_frame = WatchList(self.binance.contracts, self.bitmex.contracts,
+                                          self._left_frame, bg=BG_COLOR)
+        self._watchlist_frame.pack(side=tk.TOP)
+
         self._logging_frame = Logging(self._left_frame, bg=BG_COLOR)
         self._logging_frame.pack(side=tk.TOP)
 
         self._update_ui()
 
     def _update_ui(self):
+
+        # Logs
 
         for log in self.bitmex.logs:
             if not log["displayed"]:
@@ -46,4 +53,16 @@ class Root(tk.Tk):
                 self._logging_frame.add_log(log['log'])
                 log['displayed'] = True
 
+        # Watchlist Prices
+
+        for key, value in self._watchlist_frame.body_widgets['Symbol'].items():
+            symbol = self._watchlist_frame.body_widgets['Symbol'][key].cget('text')
+            exchange = self._watchlist_frame.body_widgets['Exchange'][key].cget('text')
+
+            if exchange == "Binance":
+                if symbol not in self.binance.contracts:
+                    continue
+                if symbol not in self.binance.prices:
+                    continue
+                
         self.after(1500, self._update_ui)
